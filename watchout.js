c97fcb69bf0d8d9cad5e2b35f26453da2ef78d91
radicalsauce <krhale@gmail.com>
2014-09-20 16:41:58 -0700

    With colors

--- watchout.js
+++ watchout.js
@@ -1,5 +1,5 @@
-var numCircles = 2;
-var width = 20, height = 20;
+var numCircles = 50;
+var width = 40, height = 40;
 var currentScore = 0;
 var highScore = 0;
 var collisions = 0;
@@ -23,17 +23,17 @@ var drag = d3.behavior.drag()
         .tween('text',function(){
           return function(t){
             d3.selectAll('circle').data(Array(numCircles)).attr('fill',function(data){
-                var color;
-                var xDist = Math.pow(d3.select('rect').attr('x') - d3.select(this).attr('cx'), 2);
-                var yDist = Math.pow(d3.select('rect').attr('y') - d3.select(this).attr('cy'), 2);
+                var color = d3.select(this).attr('fill');
+                var xDist = Math.pow(d3.select('rect').attr('x')+10 - d3.select(this).attr('cx'), 2);
+                var yDist = Math.pow(d3.select('rect').attr('y')+10 - d3.select(this).attr('cy'), 2);
                 var distance = Math.sqrt(xDist + yDist);
-                if(distance < 20) {
+                if(distance < 30) {
                   currentScore=0;
-                  color = 'blue';
+                  color = 'black';
                 }
                 if(color==='blue' && distance >= 20){
                   upCollisions();
-                  color = 'black';
+                  collisions++;
                 }
 
                 return color;
@@ -64,15 +64,17 @@ var moveCircles = function(){
       .transition()
         .attr('cx', function(){return randomX()})
         .attr('cy', function(){return randomY()})
-        .tween('text',function(){
+        .tween('fill',function(){
           var collided = false;
           return function(t){
-            var xDist = Math.pow(d3.select(this).attr('cx') - d3.select('rect').attr('x'), 2)
-            var yDist = Math.pow(d3.select(this).attr('cy') - d3.select('rect').attr('y'), 2)
+            var xDist = Math.pow(d3.select(this).attr('cx') - d3.select('rect').attr('x')-10, 2)
+            var yDist = Math.pow(d3.select(this).attr('cy') - d3.select('rect').attr('y')-10, 2)
             var distance = Math.sqrt(xDist + yDist);
 
-            if(distance < 20) {
+            if(distance < 30) {
               currentScore=0;
+              var colors = ['blue','red','yellow','green']
+              d3.select(this).attr('fill', colors[Math.floor(Math.random()*4)])
               collided = true;
             }
             if(collided === true && distance >=20){
@@ -99,5 +101,5 @@ var upCollisions = function(){
   d3.select('.collisions').text('Collisions: '+ collisions);
 }
 setInterval(upScore, 100);
-// setInterval(moveCircles, 2500);
+setInterval(moveCircles, 1500);
 
